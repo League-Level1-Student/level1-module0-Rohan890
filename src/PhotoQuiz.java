6ae8b05df6d18af44e26731435c0056c0bdc4c41
Rohan890 <league@WTS-iMac-11.attlocal.net>
2018-09-23 15:55:33 -0700

    photoquiz/MyfirstswingGUI

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,33 +22,45 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+String u = ("https://images-na.ssl-images-amazon.com/images/I/41BjwT0HeUL._SX425_.jpg");
 		// 2. create a variable of type "Component" that will hold your image
-
+Component r;
 		// 3. use the "createImage()" method below to initialize your Component
-
+r = createImage(u);
 		// 4. add the image to the quiz window
-
+quizWindow.add(r);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String q = JOptionPane.showInputDialog("What city does the picture represent");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(q.equals("Los Angeles")) {
+	System.out.println("CORRECT");
+}
+else {
+	System.out.println("INCORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+quizWindow.remove(r);
 		// 10. find another image and create it (might take more than one line of code)
-
+String p = ("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/SDPadres_logo.svg/1200px-SDPadres_logo.svg.png");
 		// 11. add the second image to the quiz window
-
+Component t;
+t = createImage(p);
+quizWindow.add(t);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String h = JOptionPane.showInputDialog("What team is this");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+if(h.equals("Padres")) {
+	System.out.println("CORRECT");
+}
+else {
+	System.out.println("INCORRECT");
+	}
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
