01b737f8a5bb644764ebaa2bccfe1ad72f4fe773
munchman <nootnoot@gmail.com>
2018-10-14 15:55:08 -0700

    fortune cookie

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,8 +1,38 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class FortuneCookie {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
+public class FortuneCookie implements ActionListener {
+public static void main(String[] args) {
+	FortuneCookie fc = new FortuneCookie();
+	fc.showButton();
+}
+	
 	public FortuneCookie() {
 		// TODO Auto-generated constructor stub
 	}
+	public void showButton() {
+	     System.out.println("Button clicked");
+	     JFrame frame = new JFrame();
+
+	     frame.setVisible(true);
+	     JButton button = new JButton();
+	     frame.add(button);
+	     button.addActionListener(this);
+	     frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+JOptionPane.	showMessageDialog(null,"Woohoo");
+int rand = new Random().nextInt(5);
+
+
+}
 
 }
---
+++ src/JackintheBox.java
@@ -0,0 +1,11 @@
+import javax.swing.JButton;
+
+public class JackintheBox {
+public static void main(String[] args) {
+	JButton suprise = new JButton();
+}
+	public JackintheBox() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
