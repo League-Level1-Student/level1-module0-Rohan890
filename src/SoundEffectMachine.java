e8e66fe72b3ab6b24043c511ceed336dbd325d1e
munchman <nootnoot@gmail.com>
2018-10-21 15:55:52 -0700

    jackinrhwboxsoundeffectfortunecookue

---
+++ src/SoundEffectMachine.java
@@ -0,0 +1,19 @@
+import java.nio.charset.MalformedInputException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SoundEffectMachine {
+public static void main(String[] args) {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton leftButton = new JButton();
+	JButton rightButton = new JButton();
+	
+}
+	public SoundEffectMachine() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
